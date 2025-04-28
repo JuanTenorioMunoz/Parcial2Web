dd340041cda65276bbe3ca5c70294d547c8a2788
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-04-28 18:26:57 -0500

    add loading state

--- parcial2/src/pages/Home.jsx
+++ parcial2/src/pages/Home.jsx
@@ -6,26 +6,34 @@ const Home = () => {
 
     const [pokemon, setPokemon] = useState("")
     const [pokeQuery, setPokeQuery] = useState("pikachu")
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
         fetchPokemonByName(pokeQuery)
         .then(response => {
             setPokemon(response)
-            console.log(response)})
+            console.log(response)
+            setLoading(false)})
         console.log(pokemon + "pokiminowe")
     }, [])
 
-//HP, ataque, defensa, nombre
+    return (
+		<div>
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<div>
+					<h1>{pokemon.name}</h1>
+                    <p>HP: {pokemon.stats[0].base_stat}</p>
+                    <p>HP: {pokemon.stats[1].base_stat}</p>
+                    <p>HP: {pokemon.stats[2].base_stat}</p>
+                    <p>HP: {pokemon.stats[3].base_stat}</p>
+				</div>
+			)}
+		</div>
+	);
 
-    return(
-        <>
-            <h1>{pokemon.name}</h1>
-            <p>HP: {pokemon.stats[0].base_stat}</p>
-            <p>HP: {pokemon.stats[1].base_stat}</p>
-            <p>HP: {pokemon.stats[2].base_stat}</p>
-            <p>HP: {pokemon.stats[3].base_stat}</p>
-        </>
-    )
+    
 }
 
 export default Home;
