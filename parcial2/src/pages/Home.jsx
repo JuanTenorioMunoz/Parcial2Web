c07c99b3c21f303ee2462d34cf09a6417c1e40d1
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-04-28 17:41:04 -0500

    working basic fetch

--- parcial2/src/pages/Home.jsx
+++ parcial2/src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import { fetchPokemonByName } from "../services/FetchPokemon";
+
 const Home = () => {
+
+    const [pokemon, setPokemon] = useState("")
+    const [pokeQuery, setPokeQuery] = useState("pokemon/ditto")
+
+    useEffect(()=>{
+        fetchPokemonByName(pokeQuery)
+        .then(response => {
+            setPokemon(response)
+            console.log(response)})
+        console.log(pokemon + "pokiminowe")
+    }, [])
+
     return(
         <>
-        
         <p>HOME</p>
 
         </>
--- parcial2/src/services/FetchPokemon.jsx
+++ parcial2/src/services/FetchPokemon.jsx
@@ -1,1 +1,7 @@
-const apiURL = ""
+const apiURL = "https://pokeapi.co/api/v2/"
+
+export const fetchPokemonByName = (name) => {
+    return fetch(`https://pokeapi.co/api/v2/${name}`)
+    .then((response) => response.json())
+}
+
